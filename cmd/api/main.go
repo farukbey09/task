402d6d7525773faa5a89ba0c8a59dab9919a6dde
farukbey09 <farukonder2204@gmail.com>
2022-10-15 15:47:02 +0300

    final

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,13 +1,10 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/farukbey09/task/handler"
 	"github.com/farukbey09/task/rabbitmq"
 	"github.com/farukbey09/task/redis"
 	"github.com/gin-gonic/gin"
-	"github.com/streadway/amqp"
 )
 
 func main() {
@@ -15,26 +12,9 @@ func main() {
 	redisClient := redis.NewRedisClient("localhost:6379")
 	rabbitmqClient := rabbitmq.NewRabbitmqClient("amqp://user:password@localhost:7001")
 	handler := handler.NewHandler(redisClient, rabbitmqClient)
-	conn, err := amqp.Dial("amqp://user:password@localhost:7001")
-	if err != nil {
-		fmt.Println(err)
-		panic(err)
-	}
-	defer conn.Close()
-
-	fmt.Println("succesfully connected to rabbitmq instance")
-	ch, err := conn.Channel()
-	if err != nil {
-		fmt.Println(err)
-		panic(err)
-	}
-	defer ch.Close()
-
-	r.GET("/test", func(c *gin.Context) {
-		c.JSON(200, "worked")
-	})
 
 	r.POST("/message", handler.HandleAddMessage)
+	r.GET("/message/list", handler.HandleGetMessages)
 
 	r.Run()
 }
